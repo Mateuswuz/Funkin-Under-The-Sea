9ec9a43c06b0e4b1916a112d49ae988cd0966ac6
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2024-02-19 14:23:15 -0300

    Add files via upload

---
+++ assets/preload/scripts/darker.lua
@@ -0,0 +1,5 @@
+function onUpdate()
+    setProperty('boyfriend.color', getColorFromHex('BEBEBE'))
+    setProperty('dad.color', getColorFromHex('BEBEBE'))
+    setProperty('gf.color', getColorFromHex('BEBEBE'))
+end
---
+++ assets/preload/scripts/video in the game over.lua
@@ -0,0 +1,3 @@
+function onGameOverStart()
+         startVideo('dead')
+end
